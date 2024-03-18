7d721aa4c9ac97dd04ea8f430a6051f69c6fad8c
Yohannes Takata <yohannestakata1@gmail.com>
2024-03-18 16:38:34 +0300

    error controller and middleware

--- app.js
+++ app.js
@@ -3,11 +3,24 @@ const morgan = require("morgan");
 const dotenv = require("dotenv");
 const cors = require("cors");
 
+const globalErrorHandler = require("./controllers/errorController");
+const AppError = require("./utils/AppError");
+
 dotenv.config();
 
 const app = express();
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(cors());
+
+app.use("/auth", (req, res) => {
+  console.log("hi");
+});
+
+app.all("*", (req, res, next) => {
+  next(next(new AppError(`Can't find ${req.originalUrl} on this server`), 404));
+});
+
+app.use(globalErrorHandler);
 
 module.exports = app;
---
+++ controllers/errorController.js
@@ -0,0 +1,42 @@
+const AppError = require("../utils/AppError");
+
+function sendError(err, res) {
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+  } else {
+    res.status(500).json({
+      status: "error",
+      message: "Something went wrong",
+    });
+  }
+}
+
+function handleDbDuplicateField(err) {
+  const duplicateField = [
+    .../'(.*?)'/.exec(err.message.split(" ").at(-1)),
+  ][1].split(".")[1];
+
+  return new AppError(
+    `Duplicate entry on field: ${duplicateField}. Please use a different value`,
+    400
+  );
+}
+
+handleJwtError = () => new AppError("Invalid token. Please login again", 401);
+handleJwtTokenExpiredError = () =>
+  new AppError("Your login session has expired. Please login again", 401);
+
+module.exports = (err, req, res, next) => {
+  console.log(err);
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || "fail";
+
+  // Duplicate fields error
+  if (err.code === "ER_DUP_ENTRY") err = handleDbDuplicateField(err);
+  if (err.name === "JsonWebTokenError") err = handleJwtError();
+  if (err.name === "TokenExpiredError") err = handleJwtTokenExpiredError();
+  sendError(err, res);
+};
