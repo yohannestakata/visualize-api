6f35af09673e2949013bd41679c397214ac6cefb
Yohannes Takata <yohannestakata1@gmail.com>
2024-05-25 18:51:25 +0300

    multer for receiving files

--- routes/modelsRoutes.js
+++ routes/modelsRoutes.js
@@ -1,5 +1,11 @@
 import { Router } from "express";
+import { uploadModel } from "../controllers/modelController";
+import multer from "multer";
+
+const upload = multer({ dest: "uploads/" });
 
 const router = new Router();
+
+router.route("/upload").post(upload.any(), uploadModel);
 
 export default router;
