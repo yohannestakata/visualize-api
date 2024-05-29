0ed9ebe950bae03c363f48e5d76e5a7f9ca323d3
Yohannes Takata <yohannestakata1@gmail.com>
2024-05-29 06:54:05 +0300

    fixed code spacing

--- app.js
+++ app.js
@@ -20,16 +20,13 @@ const app = express();
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(cookieParser());
-
 app.use(userParser());
-
 app.use(
   cors({
     origin: "http://localhost:5173",
     credentials: true,
   })
 );
-
 app.use("/auth", authRouter);
 app.use("/users", userRouter);
 app.use("/models", modelRouter);
