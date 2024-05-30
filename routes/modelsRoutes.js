a3f83462961842e59e6569ed6f6fda62f601f8ec
Yohannes Takata <yohannestakata1@gmail.com>
2024-05-30 22:57:19 +0300

    feat: created new route models/:id

--- routes/modelsRoutes.js
+++ routes/modelsRoutes.js
@@ -1,5 +1,9 @@
 import { Router } from "express";
-import { getModels, uploadModel } from "../controllers/modelController";
+import {
+  getModels,
+  uploadModel,
+  getModel,
+} from "../controllers/modelController";
 import multer from "multer";
 
 const upload = multer();
@@ -7,5 +11,6 @@ const upload = multer();
 const router = new Router();
 
 router.route("/").post(uploadModel).get(getModels);
+router.route("/:id").get(getModel);
 
 export default router;
