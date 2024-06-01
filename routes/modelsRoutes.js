3e21c606f9e6fe36cd7a28ec4db0dcef5ca8d455
Yohannes Takata <yohannestakata1@gmail.com>
2024-06-01 21:09:05 +0300

    feat: created update model route

--- routes/modelsRoutes.js
+++ routes/modelsRoutes.js
@@ -3,14 +3,13 @@ import {
   getModels,
   uploadModel,
   getModel,
+  updateModel,
 } from "../controllers/modelController";
 import multer from "multer";
-
-const upload = multer();
 
 const router = new Router();
 
 router.route("/").post(uploadModel).get(getModels);
-router.route("/:id").get(getModel);
+router.route("/:id").get(getModel).patch(updateModel);
 
 export default router;
