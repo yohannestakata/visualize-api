d2b1f649258f95d2a169933960bae76a7b3bd1af
Yohannes Takata <yohannestakata1@gmail.com>
2024-06-02 09:42:23 +0300

    tests

--- api/index.js
+++ api/index.js
@@ -1,6 +1,8 @@
 import app from "../app";
 import mongoose from "mongoose";
 
+console.log(process.env.NODE_ENV);
+
 app.listen(process.env.PORT, () => {
   console.log(`Listening on port ${process.env.PORT}`);
 });
--- app.js
+++ app.js
@@ -28,7 +28,7 @@ if (process.env.NODE_ENV === "development")
       credentials: true,
     })
   );
-else
+if (process.env.NODE_ENV === "production")
   app.use(
     cors({
       origin: "https://visualize-tau.vercel.app",
