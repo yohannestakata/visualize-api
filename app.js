c64847cfb750da931fb6309f2d921756bde5bfb2
Yohannes Takata <yohannestakata1@gmail.com>
2024-06-02 22:01:04 +0300

    change domain

--- app.js
+++ app.js
@@ -50,6 +50,7 @@ if (process.env.NODE_ENV === "production")
     cors({
       origin: "https://visualize-qakf.onrender.com",
       credentials: true,
+      exposedHeaders: ["set-cookie"],
     })
   );
 // app.use(cors());
