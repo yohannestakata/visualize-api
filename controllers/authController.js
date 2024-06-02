101d6e89f2e504854101c25f3fb37b9ccf991346
Yohannes Takata <yohannestakata1@gmail.com>
2024-06-02 20:56:14 +0300

    no more http only cookies

--- controllers/authController.js
+++ controllers/authController.js
@@ -19,7 +19,7 @@ function createSendToken(user, statusCode, req, res) {
     expires: new Date(
       Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
     ),
-    httpOnly: true,
+    // httpOnly: true,
     // secure: req.secure || req.headers["x-forwarded-proto"] === "https",
   });
 
