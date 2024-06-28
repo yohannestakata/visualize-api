ec4830d52f7b0b35fbcaef51b210b4247920f2e3
Yohannes Takata <yohannestakata1@gmail.com>
2024-06-28 07:00:40 +0300

    update department route

--- routes/departmentRoutes.js
+++ routes/departmentRoutes.js
@@ -2,10 +2,12 @@ import { Router } from "express";
 import {
   createDepartment,
   getAllDepartments,
+  updateDepartment,
 } from "../controllers/departmentController";
 
 const router = new Router();
 
+router.route("/:id").patch(updateDepartment);
 router.route("/").post(createDepartment).get(getAllDepartments);
 
 export default router;
