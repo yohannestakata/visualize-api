5e1614d2a38daecd645748625fc94ce6320d2a34
Yohannes Takata <yohannestakata1@gmail.com>
2024-06-28 07:00:12 +0300

    handle no unfound department error

--- controllers/departmentController.js
+++ controllers/departmentController.js
@@ -1,4 +1,5 @@
 import Departments from "../models/departmentsModel";
+import AppError from "../utils/AppError";
 import catchAsync from "../utils/catchAsync";
 
 export const createDepartment = catchAsync(async (req, res) => {
@@ -9,4 +10,16 @@ export const createDepartment = catchAsync(async (req, res) => {
 export const getAllDepartments = catchAsync(async (req, res) => {
   const departments = await Departments.find();
   res.status(200).json({ status: "success", data: departments });
+});
+
+export const updateDepartment = catchAsync(async (req, res) => {
+  const updatedDepartment = await Departments.findOneAndUpdate(
+    { _id: req.params.id },
+    req.body,
+    { new: true }
+  );
+  if (!updatedDepartment)
+    throw new AppError(`No department with id: ${req.params.id} found`, 401);
+
+  res.status(200).json({ status: "success", data: updatedDepartment });
 });
