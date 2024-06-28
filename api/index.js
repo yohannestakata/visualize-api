58b9e7cd9dce3567c12b7abe3274b1b1eb6e2393
Yohannes Takata <yohannestakata1@gmail.com>
2024-06-28 06:15:29 +0300

    remove console.log

--- api/index.js
+++ api/index.js
@@ -1,8 +1,6 @@
 import app from "../app";
 import mongoose from "mongoose";
 
-console.log(process.env.NODE_ENV);
-
 app.listen(process.env.PORT, () => {
   console.log(`Listening on port ${process.env.PORT}`);
 });
