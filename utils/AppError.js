d910177939b39a46482009991828301aec009022
Yohannes Takata <yohannestakata1@gmail.com>
2024-06-28 06:16:39 +0300

    fix error and fail messages

--- utils/AppError.js
+++ utils/AppError.js
@@ -2,7 +2,7 @@ class AppError extends Error {
   constructor(message, statusCode) {
     super(message);
     this.statusCode = statusCode;
-    this.status = `${statusCode}`.startsWith("4") ? "fail" : "error";
+    this.status = `${statusCode}`.startsWith("4") ? "error" : "fail";
     this.isOperational = true;
 
     Error.captureStackTrace(this, this.constuctor);
