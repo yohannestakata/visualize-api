bdc2df559994c55ae8c7f7426081d4f1e711347a
Yohannes Takata <yohannestakata1@gmail.com>
2024-06-30 20:23:45 +0300

    populate department on get user

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,7 +3,7 @@ const AppError = require("../utils/AppError");
 const catchAsync = require("../utils/catchAsync");
 
 const getUser = catchAsync(async (req, res, next) => {
-  const user = await User.findById(req.params.id);
+  const user = await User.findById(req.params.id).populate("Departments");
   if (!user) return next(new AppError("No user found", 404));
   return res.status(200).json({ status: "success", user });
 });
