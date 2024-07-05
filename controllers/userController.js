d14b3864a45f48a87f38cf6f6b529c967d285a41
Yohannes Takata <yohannestakata1@gmail.com>
2024-07-05 03:48:32 +0300

    refactor to esm

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,11 +1,18 @@
-const User = require("../models/userModel");
-const AppError = require("../utils/AppError");
-const catchAsync = require("../utils/catchAsync");
+import User from "../models/userModel";
+import AppError from "../utils/AppError";
+import catchAsync from "../utils/catchAsync";
+import { genSalt, hash } from "bcryptjs";
 
-const getUser = catchAsync(async (req, res, next) => {
-  const user = await User.findById(req.params.id).populate("Departments");
+export const getUser = catchAsync(async (req, res, next) => {
+  const user = await User.findById(req.params.id).populate("department");
   if (!user) return next(new AppError("No user found", 404));
   return res.status(200).json({ status: "success", user });
 });
 
-module.exports = { getUser };
+export const createUser = catchAsync(async (req, res) => {
+  const salt = await genSalt(10);
+  const password = await hash(req.body.password, salt);
+
+  const user = await User.create({ ...req.body, password });
+  res.status(200).json({ status: "success", data: user });
+});
