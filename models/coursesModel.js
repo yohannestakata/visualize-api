6f7f7611d847ba6ad169bbc341d17d251ce1c94d
Yohannes Takata <yohannestakata1@gmail.com>
2024-07-05 03:49:08 +0300

    add fields and validation

--- models/coursesModel.js
+++ models/coursesModel.js
@@ -1,11 +1,19 @@
 import { Schema, model } from "mongoose";
 
 const coursesSchema = Schema({
-  name: { type: String, unique: true },
-  credits: {
+  name: {
+    type: String,
+    required: true,
+  },
+  creditHour: {
     type: Number,
     min: [1, "Credits can't be less than 1"],
     max: [5, "Credits can't be more than 5"],
+    required: true,
+  },
+  number: {
+    type: String,
+    required: true,
   },
 });
 
